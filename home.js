c9083734b11bbedc382af99381f082bb3b6f4989
real-syjo <csy815kr@gmail.com>
2021-05-17 19:43:20 +0900

    navbar menu action

--- home.js
+++ home.js
@@ -15,8 +15,16 @@ document.addEventListener('scroll', ()=>{
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event)=>{
-    console.log("zzzz");
-})
+    const target = event.target;
+	const link = target.dataset.link;
+	
+	if(link == null){
+		return;
+	}
+
+	const scrollTo = document.querySelector(`${link}`);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+});
 
 const elts = {
 	text1: document.getElementById("text1"),
